c8bb32ab9aebcf1a81e638949b865f29a857b836
Maisie McGhee <maisiemcghee@tests-MacBook-Pro.local>
2023-10-25 11:39:12 +0100

    voter functionality corrected

--- src/components/Voter.jsx
+++ src/components/Voter.jsx
@@ -1,33 +1,34 @@
 import { useState } from "react"
+import { updateArticleVotes } from "../api"
+import { useParams } from "react-router-dom"
 
 function Voter({ likes }) {
-    const [userLikes, setUserLikes] = useState(0);
+     const [userLikes, setUserLikes] = useState(0)
+     const { article_id } = useParams()
 
-    const updateLikes = (value) => {
+     const updateVote = (value) => {
         setUserLikes((currentLikes) => {
-            return currentLikes + value;
+            return currentLikes + value
         })
-    }
+        updateArticleVotes(article_id, value).catch(() => {
+            setUserLikes((currentLikes) => {
+                return currentLikes - value
+            })
+        })
+     }
 
     return (
         <div>
-            <p className="article-votes">Votes : {likes + userLikes} <button className="like-button"
-                disabled={userLikes === 1}
-                aria-label="like"
-                onClick={() => {
-                    updateLikes(1)
-                }}>
-                    +
-                </button>
-                <button className="dislike-button"
-                disabled={userLikes === -1}
-                aria-label="dislike"
-                onClick={() => {
-                    updateLikes(-1)
-                }}>
-                    -
-                </button>
-                </p>
+            <p className="voter" >Votes : {likes + userLikes} <button className="like-button"
+            disabled={userLikes === 1}
+            onClick={() => {
+                updateVote(1)
+            }}>+</button>
+            <button className="dislike-button"
+            disabled={userLikes === -1}
+            onClick={() => {
+                updateVote(-1)
+            }}>-</button></p>
         </div>
     )
 }
